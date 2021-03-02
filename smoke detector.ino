a5a3b9a11a365cda871f149df7bd72fb50d62282
vanshkarnwal13 <76529617+vanshkarnwal13@users.noreply.github.com>
2021-03-02 16:04:52 +0530

    Update smoke detector.ino

--- smoke detector.ino
+++ smoke detector.ino
@@ -1,3 +1,45 @@
+#include <LiquidCrystal.h>
+
+
+int buzzer = 4;
+int gassensor = A0;
+int sensorThresh = 400;
+
+
+void setup()
+{
+
+pinMode(buzzer,OUTPUT);
+
+pinMode(gassensor,INPUT);
+Serial.begin(9600);
+
+}
+
+void loop()
+{
+  
+  int analogValue = analogRead(gassensor);
+  Serial.println(analogValue); 
+  
+  if(analogValue>sensorThresh)
+  {
+    
+    tone(buzzer,1000,1000);
+    
+  }
+  else
+  {
+    
+    noTone(buzzer);
+   
+  }
+  
+}
+
+
+
+
 void setup()
 {
   
